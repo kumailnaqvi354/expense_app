1e7bb7594f2980f1b22f3b51dd129d5e6c6ca75c
Kumail Naqvi <57100826+kumailnaqvi354@users.noreply.github.com>
2020-08-23 20:00:35 +0500

    Revert "first git push"
    This reverts commit 3b4c00d35266f6a62ea8fb8369b16bb337e63013.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Child from './child.js';
-import {TransactionProvider} from './transContext.js';
 
 function App() {
   return (
-    <TransactionProvider>
-      <Child />      
-    </TransactionProvider>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
